14d7830d3be0ae1bd7324b9ec0c1ba06f1b56289
raldybn <raldybelo@gmail.com>
2024-01-24 20:20:53 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#list
+def maka(x,y):
+    return [-18*x/y,1/2*x+y]
+jika_1=10
+jika_2=4
+
+hasil=maka(jika_1,jika_2)
+print(f"hasil dari-18.x/y adalah {hasil[0]}, lalu hasil dari 1/2*x+y adalah {hasil[1]}")
+
+#tuple
+def maka(x,y):
+    return (-18*x/y,1/2*x+y)
+jika_1=10
+jika_2=4
+hasil=maka(jika_1,jika_2)
+print(f"hasil dari-18.x/y adalah {hasil[0]}, lalu hasil dari 1/2*x+y adalah {hasil[1]}") 
