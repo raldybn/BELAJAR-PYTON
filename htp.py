a66ba62759ae83c898965095c5f191ff0eb74a08
raldybn <raldybelo@gmail.com>
2024-01-25 20:54:43 +0700

    update match

--- htp.py
+++ htp.py
@@ -1,10 +1,4 @@
-a=15
-b=-15
-c=0
-if a<b:
-    c=20
-elif a>b:
-    c=-20
-else:
-   c=(f"sama dengan")
-print(f"nilai c adalah {c}")
+print('##  Program Python Persegi Bintang  ##')
+print('======================================')
+print()
+print("helloween")
