633f9ce514e4e639729f46b987375b926f74ce8e
raldybn <raldybelo@gmail.com>
2024-01-25 16:23:35 +0700

    mantep

--- htp.py
+++ htp.py
@@ -1,6 +1,10 @@
-nama= "angga"
-
-if len(nama)==5:
-    print("mantep")
+a=15
+b=-15
+c=0
+if a<b:
+    c=20
+elif a<b:
+    c=-20
 else:
-    print ("gk mantep")
+   c=(f"sama dengan")
+print(f"nilai c adalah {c}")
