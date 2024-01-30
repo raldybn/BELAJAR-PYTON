3652a7b42acc7b7b7d6921e0ab19df798f51c99c
raldybn <raldybelo@gmail.com>
2024-01-30 08:58:35 +0700

    belajar class

--- htp.py
+++ htp.py
@@ -1,4 +1,10 @@
-print('##  Program Python Persegi Bintang  ##')
-print('======================================')
-print()
-print("helloween")
+makanan = input('masukan nama makan') # "sushi"
+
+makanan_tersedia = ["pizza", "sushi", "burger"]
+
+print(f"makanan yang dimasukan {makanan}")
+if makanan in makanan_tersedia:
+    print("tersedia")
+else:
+    print("tidak ada di menu")
+
